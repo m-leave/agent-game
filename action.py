fbc62bcc21af871301ee0c1633643f8bcb3fe645
mleaves <36160783+m-leave@users.noreply.github.com>
2023-09-11 12:44:47 +0800

    Update action.py

--- action.py
+++ action.py
@@ -15,6 +15,10 @@ with open('short_memory.json') as f:
 with open('summary_memory.json') as f:
     summary_memory = json.load(f)
 
+# load the recent events
+with open('recent_events.json') as f:
+    recent_events = json.load(f)
+
 logs = [
     'LogBlueprintUserMessages: [BP_TestAILogPlayer_C_0] Info from [BP_TestAILogPlayer_C_0] : Tag Als.Gait.Running Added! Time: 1.624091',
     'LogBlueprintUserMessages: [BP_TestAILogPlayer_C_0] Info from  [BP_TestAILogPlayer_C_0] :  Tag Als.Gait.Sprinting Removed! Time: 36.894782',
@@ -26,7 +30,7 @@ logs = [
     'Component BP_Paddle0.PLStaticMesh TreeBranch with tag Collision.Paddle in actor BP_Paddle0 with tag Entity.Paddle leaved 2.5m! Time: 11.758633'
 ]
 
-os.environ["OPENAI_API_KEY"] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+os.environ["OPENAI_API_KEY"] = "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"
 api_key = os.environ["OPENAI_API_KEY"]
 llm = OpenAI()
 
@@ -126,6 +130,54 @@ def predict_response():
     return possible_conv.generations[0][0].text
 
 
+def reflection():
+    """
+    reflect and update the summary_memory.json
+    :return: new memory
+    """
+    prompt_template = """
+        Persona name:{persona_name}
+        Age:{age}
+        Gender:{gender}
+        Job:{job}
+
+        Summary of persona's history: 
+        {persona_history}
+
+        events happened recently:
+        {events_recently}
+
+        Based on the information provided, summary three most important conclusions for {persona_name} and return the result with json : 
+        """
+
+    persona_name = basic_memory['name']
+    age = basic_memory['age']
+    gender = basic_memory['gender']
+    job = basic_memory['job']
+    persona_history = summary_memory
+    # for event in recent_events:
+    #     events_recently = events_recently + event
+    events_recently = recent_events
+
+    prompt = prompt_template.format(
+        persona_name=persona_name,
+        age=age,
+        gender=gender,
+        job=job,
+        persona_history=persona_history,
+        events_recently=events_recently
+    )
+
+    result = llm.generate(
+        prompts=[f"{prompt}"],
+        model="text-davinci-003",
+        api_type="open_ai"
+    )
+    print(result.generations[0][0].text)
+    return result.generations[0][0].text
+
+
 if __name__ == '__main__':
     # predict_action()
-    predict_response()
+    # predict_response()
+    reflection()
